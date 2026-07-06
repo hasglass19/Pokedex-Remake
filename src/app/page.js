3279b513f11f911ba9e295830196a800121272dd
Hasan Altayeb <hasglass19@gmail.com>
2026-07-06 12:16:52 +0300

    Individual pages created for each Pokemon

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,8 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 async function getMons() {
     const res=await fetch('https://pokeapi.co/api/v2/pokemon')
-
-    const data=await res.json();
+    const data= await res.json();
     return data.results;
-}
-
-function tableRow (idurl){
-  idurl=parseInt(idurl);
-  if (idurl%1==0)
-    return(<div></div>);
 }
 
 
@@ -20,14 +12,14 @@ export default async function Home() {
     <div className={styles.page}>
       <main className={styles.main}>
           <h1>Welcome to the Pokedex</h1>
-          {mons.map((mon)=>(<table>
+          {mons.map((mon)=>(<table key={mons.indexOf(mon)+1}>
             <tbody>
               <tr>
                 <th>Index Number: </th>
                 <th>Name: </th>
               </tr>
               <tr>
-                <td>{mon.url.substring(34,mon.url.length-1)}</td>
+                <td>{mons.indexOf(mon)+1}</td>
                 <td>{mon.name.charAt(0).toUpperCase() + mon.name.slice(1)}</td>
               </tr>
             </tbody>
--- src/app/pokemons/[id]/page.js
+++ src/app/pokemons/[id]/page.js
@@ -1,26 +1,34 @@
-async function getMon(id) {
-    const res = await fetch('https://pokeapi.co/api/v2/pokemon/'+id);
-    return await res.json();
-}
 
-export default async function PokemonDetails(params) {
-    const mons = await getMon(params)
-  return (
-    <>{
-        <div key={mons.id}>
-            <table>
-                <tbody>
-                <tr>
-                    <th>Index: </th>
-                    <th>Name: </th>
-                </tr>
-                <tr>
-                    <td>{mons}</td>
-                    <td>{mons}</td>
-                </tr>
-                </tbody>    
-            </table>
-        </div>
-    }</>
-  );
-}
+    import Image from "next/image";
+
+    async function getMon(id) {
+        const res = await fetch('https://pokeapi.co/api/v2/pokemon/'+id,{
+            next: {
+                revalidate: 60
+            }
+        });
+        return res.json();
+    }
+
+    export default async function PokemonDetails({ params }) {
+        params = await params;
+        console.log("PARAMS: ", params);
+        const mons = await getMon(params.id);
+    return (
+        <>{
+                <table>
+                    <tbody>
+                    <tr>
+                        <th>Index: </th>
+                        <th>Name: </th>
+                    </tr>
+                    <tr>
+                        <td>{mons.id}</td>
+                        <td>{mons.name}</td>
+                        <td><Image src={mons.sprites.front_default} width={200} height={200} alt="Default Sprite"/></td>
+                    </tr>
+                    </tbody>    
+                </table>
+        }</>
+    );
+    }
